17c42df56bc7bea47206754b62f1495eb955b2a6
David Sugar <dyfet@gnutelephony.org>
2011-12-12 15:40:27 -0500

    Revised configuration

---
+++ system/coastal-manpager.desktop.cmake
@@ -0,0 +1,10 @@
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Coastal Manpager
+GenericName=Manual Viewer
+Comment=Manual page viwer
+TryExec=${CMAKE_INSTALL_PREFIX}/bin/coastal-manpager
+Exec=${CMAKE_INSTALL_PREFIX}/bin/coastal-manpager &
+Categories=Application;Utility;
+Icon=${CMAKE_INSTALL_PREFIX}/share/icons/coastal-manpager.png
---
+++ system/libcoastal.pc.cmake
@@ -0,0 +1,10 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+libdir=${CMAKE_INSTALL_PREFIX}/lib
+includedir=${CMAKE_INSTALL_PREFIX}/include
+
+Name: libcoastal
+Description: coastal library
+Version: ${COASTAL_VERSION}
+Libs: -lcoastal
+CFlags:
+Requires: QtCore QtGui
