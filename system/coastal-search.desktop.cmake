216c8ff8f1e5d7289437be653947c90267072ad7
David Sugar <dyfet@gnutelephony.org>
2012-06-16 14:50:17 -0400

    more work on adding coastal-search

---
+++ system/coastal-search.desktop.cmake
@@ -0,0 +1,10 @@
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Coastal Search
+GenericName=Text Search
+Comment=Text search and view
+TryExec=${CMAKE_INSTALL_PREFIX}/bin/coastal-search
+Exec=${CMAKE_INSTALL_PREFIX}/bin/coastal-search &
+Categories=Application;Utility;
+Icon=${CMAKE_INSTALL_PREFIX}/share/icons/coastal-search.png
