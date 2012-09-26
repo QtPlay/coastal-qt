54d55cdec0e74bc8c92a3b55107a256e364e7f38
David Sugar <dyfet@gnutelephony.org>
2012-09-26 08:37:56 -0400

    header fixed

--- common/coastal.h
+++ common/coastal.h
@@ -1,3 +1,23 @@
+// Copyright (C) 2011 David Sugar, Tycho Softworks
+//
+// This file is part of coastal-qt.
+//
+// Coastal-qt is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// coastal-qt is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with coastal-qt.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _COASTAL_H_
+#define _COASTAL_H_
+
 #include <QIcon>
 #include <QDialog>
 #include <QMainWindow>
@@ -122,4 +142,5 @@ public:
     static bool idle(void);
 };
 
+#endif
 
