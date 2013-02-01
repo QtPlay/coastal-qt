f235c3f99365df87bbcc816f19800963214fb8c1
David Sugar <dyfet@gnutelephony.org>
2013-02-01 08:51:51 -0500

    more cleanup

--- system/coastal-manpager.desktop.cmake
+++ system/coastal-manpager.desktop.cmake
@@ -7,4 +7,4 @@ Comment=Manual page viwer
 TryExec=${CMAKE_INSTALL_PREFIX}/bin/coastal-manpager
 Exec=${CMAKE_INSTALL_PREFIX}/bin/coastal-manpager &
 Categories=Application;Utility;
-Icon=coastal-manpager.png
+Icon=coastal-manpager
--- system/coastal-search.desktop.cmake
+++ system/coastal-search.desktop.cmake
@@ -7,4 +7,4 @@ Comment=Text search and view
 TryExec=${CMAKE_INSTALL_PREFIX}/bin/coastal-search
 Exec=${CMAKE_INSTALL_PREFIX}/bin/coastal-search &
 Categories=Application;Utility;
-Icon=coastal-search.png
+Icon=coastal-search
