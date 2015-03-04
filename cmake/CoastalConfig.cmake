3a28d1617c25c416e438c6eec985d5992cb3b104
David Sugar <dyfet@gnutelephony.org>
2015-03-04 13:50:57 -0500

    source dependencies

--- cmake/CoastalConfig.cmake
+++ cmake/CoastalConfig.cmake
@@ -59,6 +59,12 @@ function(add_library_version _LIBRARY)
     set_target_properties(${_LIBRARY} PROPERTIES VERSION ${_VERSION} SOVERSION ${_SOVERSION})
 endfunction()
 
+function(add_source_dependencies _TARGET)
+    if(NOT "${CMAKE_SOURCE_DIR}" STREQUAL "${PROJECT_SOURCE_DIR}")
+        add_dependencies(${_TARGET} ${ARGN})
+    endif()
+endfunction()
+
 function(create_specfile)
     if(ARGN)
         foreach(arg ${ARGN})
